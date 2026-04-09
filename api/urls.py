4f71c7d0975ee533fc54efa68589c8f2d0dcbb6b
Ziyodullo <ziyodullodev@gmail.com>
2026-04-09 21:07:34 +0500

    Add Mashina model, serializer, and API views for CRUD operations

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("myapi/", views.MyAPi.as_view()),
+    path("mashina/", views.MashinaApiView.as_view()),
+    path("mashina/<int:pk>/", views.MashinaDetailApiView.as_view()),
 ]
